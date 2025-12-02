1ca8ff5dba8326de7869bcf4680067e84e91e395
smiqie <smiqie9201@gmail.com>
2025-12-02 12:46:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// ==================== SIMPLE CALCULATOR ====================
+/*
+const sum = (a, b) => a + b
+const mult = (a, b) => a * b
+const sub = (a, b) => a - b
+const div = (a, b) => a / b
+
+let isContinue = null;
+
+do {
+    let firstNum = Number(prompt('Enter first number: '));
+    let secondNum = Number(prompt('Enter second number: '));
+    let operation = prompt('Enter operation (/, *, -, +): ');
+
+    switch (operation) {
+        case '+':
+            alert(`sum: ${sum(firstNum, secondNum)}`);
+            break;
+        case '-':
+            alert(`sub: ${sub(firstNum, secondNum)}`);
+            break;
+        case '/':
+            if (secondNum === 0) {
+                confirm('error!');
+                 continue;
+            }
+            else {
+                alert(`div: ${div(firstNum, secondNum)}`);
+            }
+            break;
+        case '*':
+            alert(`mult: ${mult(firstNum, secondNum)}`);
+            break;
+        default:
+            confirm('?');
+    }
+
+
+
+    isContinue = confirm('Continue?');
+} while (isContinue);
+*/
+
+// ====================  ====================
+
